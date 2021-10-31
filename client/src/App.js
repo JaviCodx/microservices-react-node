e32c97d1b672020072ae7cf3bf46ef17a46b5ac5
javier.saiz <javier.saiz@martico.com>
2021-10-31 20:46:28 +0100

    add post list

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import PostForm from "./PostForm";
+import PostList from "./Postlist";
 const App = () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostForm></PostForm>
+      <hr></hr>
+      <PostList></PostList>
     </div>
   );
 };
---
+++ client/src/Postlist.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const PostList = () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        key={post.id}
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <div className="d-flex flex-row flex-wrap justify-content-between">
+        {renderedPosts}
+      </div>
+    </div>
+  );
+};
+
+export default PostList;
