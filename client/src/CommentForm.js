14720216016348f661a10a89f2224438ae4b497d
Javier Saiz <javiersaizcano@gmail.com>
2021-11-14 14:18:00 +0100

    add k8s ingress && client depl

--- client/src/CommentForm.js
+++ client/src/CommentForm.js
@@ -5,7 +5,7 @@ const CommentForm = ({ postId }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+    await axios.post(`http://posts.com/posts/${postId}/comments`, {
       content,
     });
 
--- client/src/PostForm.js
+++ client/src/PostForm.js
@@ -5,7 +5,7 @@ const PostForm = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    await axios.post("http://localhost:4000/posts", {
+    await axios.post("http://posts.com/posts/create", {
       title,
     });
 
